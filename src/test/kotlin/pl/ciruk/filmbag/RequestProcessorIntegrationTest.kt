406eb0ae8857f3c500a65f3f099ab9ec457ffd58
c.piotre <c.piotre@gmail.com>
2018-11-12 22:18:37 +0100

    Use model class in rest test

--- src/test/kotlin/pl/ciruk/filmbag/RequestProcessorIntegrationTest.kt
+++ src/test/kotlin/pl/ciruk/filmbag/RequestProcessorIntegrationTest.kt
@@ -7,11 +7,9 @@ import org.mockito.Mockito.mock
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
-import org.springframework.http.HttpStatus
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import pl.ciruk.filmbag.boundary.FilmRequest
 import pl.ciruk.filmbag.boundary.ScoreRequest
@@ -23,41 +21,30 @@ import pl.ciruk.filmbag.request.DataLoader
         classes = [TestConfiguration::class, FilmBagApplication::class])
 class RequestProcessorIntegrationTest(@Autowired val restTemplate: TestRestTemplate) {
     @Test
-    fun `Should get created films`() {
-        val genres = listOf("Genre1", "Genre2", "Genre3")
-        val title = "Test title"
-        val year = 1912
-        val score = 0.623
-        val numberOfScores = 2
-        val link = "http://test/image.png"
+    fun `should get created films`() {
         val filmRequest = FilmRequest(
-                title = title,
-                year = year,
-                score = score,
-                numberOfScores = numberOfScores,
-                scores = listOf(ScoreRequest(0.1, 2), ScoreRequest(0.2, 3)),
-                genres = genres,
-                link = link
+                title = "Test title",
+                year = 1912,
+                score = 0.623,
+                numberOfScores = 2,
+                scores = setOf(ScoreRequest(0.1, 2), ScoreRequest(0.2, 3)),
+                genres = setOf("Genre1", "Genre2", "Genre3"),
+                link = "http://test/image.png"
         )
-        restTemplate.put("/resources/films", filmRequest)
-        val entity = restTemplate.getForEntity<List<Map<String, Any>>>("/resources/films")
-        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).hasSize(1)
-        val firstResponseObject = entity.body?.first()
-        assertThat(firstResponseObject)
-                .containsEntry("title", title)
-                .containsEntry("year", year)
-                .containsEntry("score", score)
-                .containsEntry("numberOfScores", numberOfScores)
-                .containsEntry("link", link)
-                .containsEntry("genres", genres)
+        val otherFilmRequest = FilmRequest(
+                title = "Other title",
+                year = 1999,
+                score = 0.87,
+                numberOfScores = 1,
+                scores = setOf(ScoreRequest(0.1, 10)),
+                genres = setOf("Genre2", "Genre1", "Genre4"),
+                link = "http://other/image.png"
+        )
 
-        val listOfScores = firstResponseObject?.get("scores") as List<Map<String, Any>>
-        assertThat(listOfScores)
-                .containsOnly(
-                        mapOf(Pair("grade", 0.1), Pair("quantity", 2)),
-                        mapOf(Pair("grade", 0.2), Pair("quantity", 3))
-                )
+        restTemplate.put("/resources/films", listOf(filmRequest, otherFilmRequest))
+        val arrayOfFilmRequests = restTemplate.getForObject("/resources/films", Array<FilmRequest>::class.java)
+
+        assertThat(arrayOfFilmRequests).containsOnly(filmRequest, otherFilmRequest)
     }
 }
 
