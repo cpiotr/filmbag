bd50bb5614d61ad94c00205a097910ef74461cf8
c.piotre <c.piotre@gmail.com>
2018-11-19 22:20:06 +0100

    Add integration test verifying deduplication

--- src/test/kotlin/pl/ciruk/filmbag/RequestProcessorIntegrationTest.kt
+++ src/test/kotlin/pl/ciruk/filmbag/RequestProcessorIntegrationTest.kt
@@ -10,12 +10,14 @@ import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import pl.ciruk.filmbag.boundary.FilmRequest
 import pl.ciruk.filmbag.boundary.ScoreRequest
 import pl.ciruk.filmbag.request.DataLoader
 
 @ExtendWith(SpringExtension::class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         classes = [TestConfiguration::class, FilmBagApplication::class])
@@ -41,10 +43,38 @@ class RequestProcessorIntegrationTest(@Autowired val restTemplate: TestRestTempl
                 link = "http://other/image.png"
         )
 
-        restTemplate.put("/resources/films", listOf(filmRequest, otherFilmRequest))
-        val arrayOfFilmRequests = restTemplate.getForObject("/resources/films", Array<FilmRequest>::class.java)
+        executePutRequest(filmRequest, otherFilmRequest)
+        val arrayOfFilmRequests = executeGetRequest()
 
         assertThat(arrayOfFilmRequests).containsOnly(filmRequest, otherFilmRequest)
+    }
+
+    @Test
+    fun `should not store single film twice`() {
+        val filmRequest = FilmRequest(
+                title = "Some title",
+                year = 1914,
+                score = 0.123,
+                numberOfScores = 3,
+                scores = setOf(ScoreRequest(0.1, 2), ScoreRequest(0.2, 3), ScoreRequest(0.4, 4)),
+                genres = setOf("Genre1", "Genre3"),
+                link = "http://test123/image.png"
+        )
+
+        executePutRequest(filmRequest)
+        executePutRequest(filmRequest)
+
+        val arrayOfFilmRequests = executeGetRequest()
+
+        assertThat(arrayOfFilmRequests)
+                .hasSize(1)
+                .containsOnly(filmRequest)
+    }
+
+    private fun executeGetRequest() = restTemplate.getForObject("/resources/films", Array<FilmRequest>::class.java)
+
+    private fun executePutRequest(vararg filmRequests: FilmRequest) {
+        restTemplate.put("/resources/films", filmRequests)
     }
 }
 
