6afe68c6dfacfa052ebdf67a298a8eb4720170e1
c.piotre <c.piotre@gmail.com>
2019-02-03 23:02:10 +0100

    Add tests for finding films by score range

--- src/test/kotlin/pl/ciruk/filmbag/DataModel.kt
+++ src/test/kotlin/pl/ciruk/filmbag/DataModel.kt
@@ -2,15 +2,12 @@ package pl.ciruk.filmbag
 
 import pl.ciruk.filmbag.boundary.FilmRequest
 import pl.ciruk.filmbag.boundary.ScoreRequest
-import pl.ciruk.filmbag.film.Film
-import pl.ciruk.filmbag.film.Genre
-import pl.ciruk.filmbag.film.Score
 import java.util.*
 
-fun testFilmRequest(year: Int = 1912) = FilmRequest(
+fun testFilmRequest(year: Int = 1912, score: Double = 0.623) = FilmRequest(
         title = "Test title " + UUID.randomUUID(),
         year = year,
-        score = 0.623,
+        score = score,
         numberOfScores = 4,
         scores = setOf(
                 ScoreRequest(0.7, 123456, url = "https://www.filmweb.pl/film/Narodziny+gwiazdy-2018-542576"),
@@ -22,19 +19,11 @@ fun testFilmRequest(year: Int = 1912) = FilmRequest(
         poster = "https://www.filmweb.pl/film/Narodziny+gwiazdy-2018-542576dsff",
         plot = "Historia najbardziej czarującego złodzieja w historii, który wcale nie miał ochoty na to, żeby przejść na emeryturę, z więzienia uciekał 30 razy, a rabując banki nigdy nie zapominał o byciu gentlemanem.")
 
-fun testOtherFilmRequest(year: Int = 1999) = FilmRequest(
+fun testOtherFilmRequest(year: Int = 1999, score: Double = 0.87) = FilmRequest(
         title = "Other title" + UUID.randomUUID(),
         year = year,
-        score = 0.87,
+        score = score,
         numberOfScores = 1,
         scores = setOf(ScoreRequest(0.1, 10, url = "Test other url")),
         genres = setOf("Genre2", "Genre1", "Genre4"),
         link = "http://other/image.png")
-
-fun testFilm(year: Int = 2013) = Film(
-        title = "Test title" + UUID.randomUUID(),
-        year = year,
-        link = "http://link",
-        score = 0.3141593,
-        scores = mutableSetOf(Score(grade = 0.3, quantity = 500), Score(grade = 0.4, quantity = 100)),
-        genres = mutableSetOf(Genre(name = "Thriller")))
--- src/test/kotlin/pl/ciruk/filmbag/film/FilmServiceTest.kt
+++ src/test/kotlin/pl/ciruk/filmbag/film/FilmServiceTest.kt
@@ -16,6 +16,7 @@ import pl.ciruk.filmbag.integration.TestConfiguration
 import pl.ciruk.filmbag.request.RequestProcessor
 import pl.ciruk.filmbag.testFilmRequest
 import pl.ciruk.filmbag.testOtherFilmRequest
+import java.math.BigDecimal
 
 @ExtendWith(SpringExtension::class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
@@ -26,9 +27,9 @@ internal class FilmServiceTest(
         @Autowired val requestProcessor: RequestProcessor,
         @Autowired val filmService: FilmService) {
 
-    val firstFilm = testFilmRequest(2009)
-    val secondFilm = testFilmRequest(2011)
-    val thirdFilm = testOtherFilmRequest(2012)
+    val firstFilm = testFilmRequest(2009, 0.9)
+    val secondFilm = testFilmRequest(2011, 0.7)
+    val thirdFilm = testOtherFilmRequest(2012, 0.6)
 
     @BeforeEach
     internal fun setUp() {
@@ -78,6 +79,54 @@ internal class FilmServiceTest(
     @Test
     fun `should not find films created outside specified range`() {
         val films = filmService.find(year = ClosedRange(2010, 2010))
+
+        assertThat(films.map { it.title })
+                .isEmpty()
+    }
+
+    @Test
+    fun `should find stored films by closed score range`() {
+        val films = filmService.find(score = ClosedRange(0.6.toBigDecimal(), 0.8999.toBigDecimal()))
+
+        assertThat(films.map { it.title })
+                .isEqualTo(listOf(secondFilm.title, thirdFilm.title))
+    }
+
+    @Test
+    fun `should find stored films by left closed score range`() {
+        val films = filmService.find(score = LeftClosedRange(0.7.toBigDecimal()))
+
+        assertThat(films.map { it.title })
+                .isEqualTo(listOf(firstFilm.title, secondFilm.title))
+    }
+
+    @Test
+    fun `should find stored films by right closed score range`() {
+        val films = filmService.find(score = RightClosedRange(0.89999.toBigDecimal()))
+
+        assertThat(films.map { it.title })
+                .isEqualTo(listOf(secondFilm.title, thirdFilm.title))
+    }
+
+    @Test
+    fun `should not find films with score greater than specified range`() {
+        val films = filmService.find(score = RightClosedRange(0.5999.toBigDecimal()))
+
+        assertThat(films.map { it.title })
+                .isEmpty()
+    }
+
+    @Test
+    fun `should not find films with score lower than specified range`() {
+        val films = filmService.find(score = LeftClosedRange(0.999.toBigDecimal()))
+
+        assertThat(films.map { it.title })
+                .isEmpty()
+    }
+
+    @Test
+    fun `should not find films with score outside specified range`() {
+        val films = filmService.find(score = ClosedRange(0.8.toBigDecimal(), 0.8.toBigDecimal()))
 
         assertThat(films.map { it.title })
                 .isEmpty()
